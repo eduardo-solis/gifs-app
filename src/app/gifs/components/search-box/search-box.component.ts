b9ab0a06e8704dac60c4b0f0ac2424e4f87bc655
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-07 14:46:04 -0600

    Usando el servicio en el componente search-box

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
     selector: 'gifs-search-box',
@@ -15,11 +16,16 @@ export class SearchBoxComponent {
 
     @ViewChild('txtTagInput')
     public txtInput!: ElementRef<HTMLInputElement>;
+
+    constructor( private gifsService: GifsService ){}
     
     //searchTag( newTag: string ):void {
     searchTag( ):void {
         const newTag = this.txtInput.nativeElement.value;
-        console.log({newTag});
+        //console.log({newTag});
+        this.gifsService.searchTag(newTag);
+
+        this.txtInput.nativeElement.value = '';
         
     }
 
