c371e6efb08036fd40115e61112bd80266aca5db
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-07 15:45:43 -0600

    Consumiendo un servicio por medio del HttpClientModule

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = '74LGCPfJgmTKiMy9EqR89Y6f7oivIFuV';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -29,7 +32,19 @@ export class GifsService {
 
     if(tag.length === 0) return;
     this.organizeHistory(tag);
-    //this._tagsHistory.unshift( tag );
+
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('q', tag)
+          .set('limit', 10)
+    
+    this.http.get(`${this.serviceUrl}/search`, { params })
+        .subscribe( resp => {
+          console.log(resp);
+          
+        })
+
+
   }
 
 }
